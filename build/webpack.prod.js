0fb0972d40ca5722910607b29f7c40530b85f05b
wliang <wliang@infervision.com>
2022-10-16 16:37:44 +0800

    feat: 完成基础prod打包及打包后预览

---
+++ build/webpack.prod.js
@@ -0,0 +1,9 @@
+const { merge } = require('webpack-merge');
+const baseConfig = require('./webpack.base.js');
+
+module.exports = merge(baseConfig, {
+  // 生产模式。自动包含UglifyJsPlugin、optimize.ModuleConcatenationPlugin、NoEmitOnErrorsPlugin插件
+  // ModuleConcatenationPlugin： 可以提升(hoist)或者预编译所有模块到一个闭包中，提升你的代码在浏览器中的执行速度
+  // NoEmitOnErrorsPlugin： 在编译出现错误时，使用 NoEmitOnErrorsPlugin 来跳过输出阶段。这样可以确保输出资源不会包含错误
+  mode: 'production',
+});
