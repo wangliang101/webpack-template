4547cc260f23775e69d06f95951d0da8dc459103
wliang <44824664+wangliang101@users.noreply.github.com>
2022-10-26 21:51:43 +0800

    Update babel.config.js

--- babel.config.js
+++ babel.config.js
@@ -9,8 +9,12 @@ module.exports = {
         //  "chrome": 49,
         //  "ie": 9
         // },
-        useBuiltIns: 'usage', // 根据配置的浏览器兼容,以及代码中使用到的api进行引入polyfill按需添加
-        corejs: 3, // 配置使用core-js使用的版本
+        // useBuiltIns共有三个参数：
+        // entry: 掉目标浏览器已支持的 polyfilll 模块，不管代码里有没有用到，只要目标浏览器不支持都会引入对应的 polyfilll 模块
+        // usage: 根据配置的浏览器兼容,以及代码中使用到的api进行引入polyfill按需添加
+        // 不作任何处理，也不会自动引入 polyfilll 模块,需要注意的是在 webpack 打包文件配置的 entry 中引入的 @babel/polyfill 不会根据 useBuiltIns 配置任何转换处理。
+        useBuiltIns: 'usage',
+        corejs: 3, // 配置使用core-js使用的版本,版本号看package.json中查看
       },
     ],
     '@babel/preset-react',
